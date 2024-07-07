f82a2882477c0cd83cf2361cbee4b54e37dd6f7f
Rishabh Raj Pathak <rishabhrajpathak06@gmail.com>
2024-07-07 22:11:31 +0530

    Made few Changes

--- Index.js
+++ Index.js
@@ -1,4 +1,10 @@
-const name = "Rishabh";
+const name1 = "Rishabh";
+const name2 = "Rishabh";
+const age = "20";
 console.log("connected to git");
 
 const date = "17-99-99";
+
+function isAbove18(age){
+    return age;
+}
